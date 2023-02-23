bcc40f4fe723a3a7a99f04650dbe504ac5e4f7bf
Gois-Prestataire GUSTAVO (KLX) [28212e10-35f0-4a99-af38-4d6fd950fda7] <gustavo.gois-prestataire@ca-ps.com>
2023-02-23 22:20:02 +0000

    Put

--- src/main/java/goisstudy/springframework/spring6restmvc/services/BeerServiceImpl.java
+++ src/main/java/goisstudy/springframework/spring6restmvc/services/BeerServiceImpl.java
@@ -95,4 +95,15 @@ public class BeerServiceImpl implements BeerService {
 
         return beerMap.get(id);
     }
+
+    @Override
+    public void updateBeerById(UUID beerId, Beer beer) {
+        Beer existing = beerMap.get(beerId);
+        existing.setBeerName(beer.getBeerName());
+        existing.setPrice(beer.getPrice());
+        existing.setUpc(beer.getUpc());
+        existing.setQuantityOnHand(beer.getQuantityOnHand());
+
+        beerMap.put(existing.getId(), existing);
+    }
 }
