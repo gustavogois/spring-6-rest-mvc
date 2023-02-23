906120ed0dc8f6bbbdeedff9baf00345e3d90371
Gois-Prestataire GUSTAVO (KLX) [28212e10-35f0-4a99-af38-4d6fd950fda7] <gustavo.gois-prestataire@ca-ps.com>
2023-02-23 00:46:55 +0000

    http get list beers

--- src/main/java/goisstudy/springframework/spring6restmvc/services/BeerService.java
+++ src/main/java/goisstudy/springframework/spring6restmvc/services/BeerService.java
@@ -2,8 +2,10 @@ package goisstudy.springframework.spring6restmvc.services;
 
 import goisstudy.springframework.spring6restmvc.model.Beer;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface BeerService {
     Beer getBeerById(UUID id);
+    List<Beer> listBeers();
 }
