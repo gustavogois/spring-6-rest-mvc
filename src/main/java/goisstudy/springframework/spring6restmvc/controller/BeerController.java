c02262430d16cd0cf1cb4d39a890cb0669900745
Gois-Prestataire GUSTAVO (KLX) [28212e10-35f0-4a99-af38-4d6fd950fda7] <gustavo.gois-prestataire@ca-ps.com>
2023-03-08 11:46:10 +0000

    Using Exception Handler

--- src/main/java/goisstudy/springframework/spring6restmvc/controller/BeerController.java
+++ src/main/java/goisstudy/springframework/spring6restmvc/controller/BeerController.java
@@ -9,6 +9,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -59,6 +60,11 @@ public class BeerController {
     @GetMapping(value = BEER_PATH)
     public List<Beer> listBeers(){
         return beerService.listBeers();
+    }
+
+    @ExceptionHandler(NotFoundException.class)
+    public ResponseEntity handleNotFoundException() {
+        return ResponseEntity.notFound().build();
     }
 
     @GetMapping(value = BEER_PATH_ID)
