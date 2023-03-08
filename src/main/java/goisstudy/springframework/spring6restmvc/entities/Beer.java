a245712e36e1cafd443afa5f82c97f738400bce3
Gois-Prestataire GUSTAVO (KLX) [28212e10-35f0-4a99-af38-4d6fd950fda7] <gustavo.gois-prestataire@ca-ps.com>
2023-03-08 23:02:36 +0000

    Creating JPA Entities

---
+++ src/main/java/goisstudy/springframework/spring6restmvc/entities/Beer.java
@@ -0,0 +1,40 @@
+package goisstudy.springframework.spring6restmvc.entities;
+
+import goisstudy.springframework.spring6restmvc.model.BeerStyle;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Version;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+/**
+ * Created by jt, Spring Framework Guru.
+ */
+@Getter
+@Setter
+@Builder
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+public class Beer {
+
+    @Id
+    private UUID id;
+
+    @Version
+    private Integer version;
+    private String beerName;
+    private BeerStyle beerStyle;
+    private String upc;
+    private Integer quantityOnHand;
+    private BigDecimal price;
+    private LocalDateTime createdDate;
+    private LocalDateTime updateDate;
+}
